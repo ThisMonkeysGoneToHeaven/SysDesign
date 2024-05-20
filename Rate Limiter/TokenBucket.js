0f35bcddae1a4751f646abd46adda63087759587
Anant Agarwal <anantagarwal@Anants-MacBook-Air.local>
2024-05-20 11:59:11 +0530

    added action plan for token bucket

---
+++ Rate Limiter/TokenBucket.js
@@ -0,0 +1,4 @@
+// Define a Bucket object
+// Define a Token object
+// Implement bucket refil strategy after each cycle
+// Request wrapper, this will receive the request and thent take it through the rate limiting mechanism
